8f55c8b1b4a6b6a27ce83ab8bbe99b9e61cfe3a7
Aditya Upadhyayula <supadhy6@jhu.edu>
2022-03-03 16:43:13 -0800

    updated input.js

--- input.js
+++ input.js
@@ -1,39 +1,36 @@
 var src = 'https://adibuoy23.github.io/event_segmentation/stimuli/';
 var stimuli = [
 {
-video_one: src+"ExampleA.mp4",
+video_one: "https://adibuoy23.github.io/event_segmentation/stimuli_with_audio/ExampleA.mp4",
 },
 {
-video_one: src+"0msec_clip_41_1917.mp4",
+video_one: src+"0msec_clip_41_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_0_1917.mp4",
+video_one: src+"0msec_clip_32_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_1_1917.mp4",
+video_one: src+"0msec_clip_33_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_2_1917.mp4",
+video_one: src+"0msec_clip_34_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_3_1917.mp4",
+video_one: src+"0msec_clip_35_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_4_1917.mp4",
+video_one: src+"0msec_clip_36_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_7_1917.mp4",
+video_one: src+"0msec_clip_37_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_8_1917.mp4",
+video_one: src+"0msec_clip_38_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_9_1917.mp4",
+video_one: src+"0msec_clip_39_1917_video_only.mp4",
 },
 {
-video_one: src+"0msec_clip_10_1917.mp4",
-},
-{
-video_one: src+"0msec_clip_11_1917.mp4",
+video_one: src+"0msec_clip_40_1917_video_only.mp4",
 },
 ]
