a99ec4a257bb96c3743ca3152222fe103dc9fed5
Aditya Upadhyayula <supadhy6@jhu.edu>
2023-02-14 16:20:04 -0800

    updated input files

--- input.js
+++ input.js
@@ -1,36 +1,126 @@
-var src = 'https://adibuoy23.github.io/event_segmentation/stimuli/';
+var src = 'https://adibuoy23.github.io/event_segmentation/stimuli_with_audio/';
 var stimuli = [
 {
-video_one: "https://adibuoy23.github.io/event_segmentation/stimuli_with_audio/ExampleA.mp4",
+video_one: src+"0msec_1.1.5_C1_trim_clip_1.mp4",
 },
 {
-video_one: src+"0msec_clip_41_1917_video_only.mp4",
+video_one: src+"0msec_1.1.5_C1_trim_clip_10.mp4",
 },
 {
-video_one: src+"0msec_clip_32_1917_video_only.mp4",
+video_one: src+"0msec_2.4.4_C1_trim_clip_2.mp4",
 },
 {
-video_one: src+"0msec_clip_33_1917_video_only.mp4",
+video_one: src+"0msec_3.1.10_C1_trim_clip_0.mp4",
 },
 {
-video_one: src+"0msec_clip_34_1917_video_only.mp4",
+video_one: src+"0msec_3.1.10_C1_trim_clip_1.mp4",
 },
 {
-video_one: src+"0msec_clip_35_1917_video_only.mp4",
+video_one: src+"0msec_3.1.10_C1_trim_clip_3.mp4",
 },
 {
-video_one: src+"0msec_clip_36_1917_video_only.mp4",
+video_one: src+"0msec_3.1.10_C1_trim_clip_4.mp4",
 },
 {
-video_one: src+"0msec_clip_37_1917_video_only.mp4",
+video_one: src+"0msec_3.1.10_C1_trim_clip_7.mp4",
 },
 {
-video_one: src+"0msec_clip_38_1917_video_only.mp4",
+video_one: src+"0msec_3.3.7_C2_trim_clip_8.mp4",
 },
 {
-video_one: src+"0msec_clip_39_1917_video_only.mp4",
+video_one: src+"0msec_3.4.2_C1_trim_clip_2.mp4",
 },
 {
-video_one: src+"0msec_clip_40_1917_video_only.mp4",
+video_one: src+"0msec_3.4.2_C1_trim_clip_4.mp4",
 },
+{
+video_one: src+"0msec_4.1.2_C2_trim_clip_2.mp4",
+},
+{
+video_one: src+"4.1.2_C2_trim_clip_7.mp4",
+},
+{
+video_one: src+"4.3.3_kinect_trim_clip_3.mp4",
+},
+//{
+//video_one: src+"0msec_clip_14_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_15_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_16_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_17_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_18_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_19_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_20_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_21_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_22_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_23_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_24_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_25_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_26_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_27_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_28_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_29_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_30_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_31_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_32_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_33_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_34_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_35_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_36_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_37_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_38_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_39_1917.mp4",
+//},
+//{
+//video_one: src+"0msec_clip_40_1917.mp4",
+//},
 ]
